7bb51930788d067293c4f29c346b6b87544313f9
Marco Navarro (u202123832) <u202123832@upc.edu.pe>
2026-04-28 11:09:06 -0500

    Inicial

---
+++ src/main/java/com/vitalmovs/VitalMovsApplication.java
@@ -0,0 +1,13 @@
+package com.vitalmovs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalMovsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitalMovsApplication.class, args);
+    }
+
+}
