fd54568136b2416a1d116822ce5def24704a5198
JoseLr422 <raperoelement@gmail.com>
2026-05-01 19:49:39 -0500

    Create PacienteDiscapacidadDTO.java

---
+++ src/main/java/com/vitalmovs/dtos/PacienteDiscapacidadDTO.java
@@ -0,0 +1,15 @@
+package com.vitalmovs.dtos;
+ 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+ 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PacienteDiscapacidadDTO {
+ 
+    private Long id;
+    private Long tipoDiscapacidadId;
+    private Long pacienteId;
+}
