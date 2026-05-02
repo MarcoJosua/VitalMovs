aca0f16c5da7d277afc8d054f27679d573ac6d4b
JoseLr422 <raperoelement@gmail.com>
2026-05-01 19:51:20 -0500

    Create TipoDiscapacidad.java

---
+++ src/main/java/com/vitalmovs/entities/TipoDiscapacidad.java
@@ -0,0 +1,26 @@
+package com.vitalmovs.entities;
+ 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+ 
+import java.util.List;
+ 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="tipo_discapacidad")
+public class TipoDiscapacidad {
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+ 
+    private String nombre;
+    private String descripcion;
+ 
+    @OneToMany(mappedBy = "tipoDiscapacidad", fetch = FetchType.EAGER)
+    private List<PacienteDiscapacidad> pacienteDiscapacidades;
+}
