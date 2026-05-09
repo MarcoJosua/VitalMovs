7d350b40a4274a81522ebb87cd63e64b88e9b5cc
algrada <132857653+algrada@users.noreply.github.com>
2026-05-08 20:24:32 -0500

    Update FisioterapeutaRepository.java

--- src/main/java/com/vitalmovs/repositories/FisioterapeutaRepository.java
+++ src/main/java/com/vitalmovs/repositories/FisioterapeutaRepository.java
@@ -3,6 +3,8 @@ package com.vitalmovs.repositories;
 import com.vitalmovs.entities.Fisioterapeuta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface FisioterapeutaRepository extends JpaRepository<Fisioterapeuta, Long> {
