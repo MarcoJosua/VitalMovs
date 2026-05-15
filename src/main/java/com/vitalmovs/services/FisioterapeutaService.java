ed702d6d53f9438b3c916dd10fa8e50f23ac5d23
algrada <132857653+algrada@users.noreply.github.com>
2026-05-15 16:31:08 -0500

    Update FisioterapeutaService.java

--- src/main/java/com/vitalmovs/services/FisioterapeutaService.java
+++ src/main/java/com/vitalmovs/services/FisioterapeutaService.java
@@ -2,6 +2,8 @@ package com.vitalmovs.services;
 
 import com.vitalmovs.dtos.FisioterapeutaDTO;
 import com.vitalmovs.entities.Fisioterapeuta;
+import com.vitalmovs.repositories.FisioterapeutaRepository;
+
 import java.util.List;
 
 public interface FisioterapeutaService {
@@ -12,4 +14,9 @@ public interface FisioterapeutaService {
     Fisioterapeuta findById(Long id);
     Fisioterapeuta update(Fisioterapeuta fisioterapeuta);
     void delete(Long id);
+    // Service interface
+    List<Fisioterapeuta> buscarPorNombreOApellido(String texto);
+    List<Fisioterapeuta> findByEspecialidad(String especialidad);
+    List<Fisioterapeuta> findByEspecialidadNative(String especialidad);
+
 }
