31f47a921003059fb8e5086f4f379fc9e852449a
algrada <132857653+algrada@users.noreply.github.com>
2026-05-15 16:31:28 -0500

    Update FisioterapeutaDiscapacidadService.java

--- src/main/java/com/vitalmovs/services/FisioterapeutaDiscapacidadService.java
+++ src/main/java/com/vitalmovs/services/FisioterapeutaDiscapacidadService.java
@@ -8,8 +8,16 @@ public interface FisioterapeutaDiscapacidadService {
     FisioterapeutaDiscapacidad add(FisioterapeutaDiscapacidad fd);
     FisioterapeutaDiscapacidadDTO addDTO(FisioterapeutaDiscapacidadDTO fdDTO);
     FisioterapeutaDiscapacidad findById(Long id);
-    List<FisioterapeutaDiscapacidad> listByFisioterapeutaId(Long fisioterapeutaId);
-    List<FisioterapeutaDiscapacidadDTO> listByFisioterapeutaIdDTO(Long fisioterapeutaId);
     FisioterapeutaDiscapacidad update(FisioterapeutaDiscapacidad fd);
     void delete(Long id);
+
+    // Query Method
+    List<FisioterapeutaDiscapacidad> listByFisioterapeutaId(Long fisioterapeutaId);
+    List<FisioterapeutaDiscapacidadDTO> listByFisioterapeutaIdDTO(Long fisioterapeutaId);
+
+    // JPQL
+    List<FisioterapeutaDiscapacidadDTO> listByTipoDiscapacidadIdDTO(Long tipoDiscapacidadId);
+
+    // Native Query — este es el que te falta
+    List<FisioterapeutaDiscapacidad> findByFisioterapeutaAndTipoNative(Long fisioterapeutaId, Long tipoDiscapacidadId);
 }
